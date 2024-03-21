4425a6369b3db14058e8ac29025538dab96c9f3c
Matthew On <146710850+matthewOn2003@users.noreply.github.com>
2024-03-21 14:58:28 +0800

    Add files via upload

---
+++ Projects/Project_TicTacToe/TicTacToe/src/main/java/com/mycompany/tictactoe/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tictactoe;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Trainee1
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        TicTacToe tictactoe = new TicTacToe();
+        
+       
+    }
+}
